af420c4ac0a301a5f38543c7c44abae8740b9ff7
Your Name <daria.dirstariu@s.unibuc.ro>
2022-05-03 04:31:43 +0300

    --navbar update

--- app/src/main/java/com/example/applicationapp/MainActivity.java
+++ app/src/main/java/com/example/applicationapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.applicationapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.SearchView;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -16,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<News> newsArrayList;
     MyAdapter myAdapter;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    FrameLayout frameLayout;
+    MaterialToolbar toolbar;
+
     String[] newHeading;
     int[] imageResourceId;
 
@@ -23,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
 
         recyclerView = findViewById(R.id.recycleView);
@@ -50,6 +67,32 @@ public class MainActivity extends AppCompatActivity {
 
         getData();
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+
     private void getData() {
         for( int i=0; i<newHeading.length; i++){
             News news = new News(newHeading[i], imageResourceId[i]);
@@ -83,4 +126,6 @@ public class MainActivity extends AppCompatActivity {
         });
         return super.onCreateOptionsMenu(menu);
     }
+
+
 }
