6529fcb0c09d716dfff3bcf78c1fb1ec2650c8dc
Stefania <vijulans@yahoo.com>
2022-05-09 19:55:01 +0300

    Gata erorile

--- app/src/main/java/com/example/applicationapp/HomeFragment.java
+++ app/src/main/java/com/example/applicationapp/HomeFragment.java
@@ -3,16 +3,106 @@ import android.os.Bundle;
 
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.SearchView;
 
 import androidx.annotation.Nullable;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+    RecyclerView recyclerView;
+    ArrayList<News> newsArrayList;
+    MyAdapter myAdapter;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    FrameLayout frameLayout;
+    MaterialToolbar toolbar;
+    View homeFr;
+    String[] newHeading;
+    int[] imageResourceId;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        setHasOptionsMenu(true);
+        homeFr = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = homeFr.findViewById(R.id.recycleView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(true);
+
+        newsArrayList = new ArrayList<News>();
+
+        newHeading = new String[]{
+                "atletism text mainActivity",
+                "basket text mainActivity",
+                "football text mainActivity","innot text mainActivity",
+                "tennis text mainActivity",
+                "volley text mainActivity"
+        };
+        imageResourceId = new int[]{
+                R.drawable.atletism,
+                R.drawable.basket,
+                R.drawable.forbal,
+                R.drawable.inot,
+                R.drawable.tenis,
+                R.drawable.volei,
+
+        };
+
+        for( int i=0; i<newHeading.length; i++){
+            News news = new News(newHeading[i], imageResourceId[i]);
+            newsArrayList.add(news);
+        }
+        System.out.println("Ceva"+ newsArrayList.size());
+
+        myAdapter = new MyAdapter(getActivity(), newsArrayList);
+        recyclerView.setAdapter(myAdapter);
+
+        myAdapter.notifyDataSetChanged();
+       // getData();
+
+        //return inflater.inflate(R.layout.fragment_home, container, false);
+        return homeFr;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater){
+     //   setHasOptionsMenu(true);
+        super.onCreateOptionsMenu(menu, menuInflater);
+        menuInflater.inflate(R.menu.menu_item, menu);
+       // getActivity().getMenuInflater().inflate(R.menu.menu_item, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_action);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+       //searchView.setMaxWidth(Integer.MAX_VALUE);
+        searchView.setQueryHint("Search Here!");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                System.out.println("newText =>"+newText);
+                myAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/applicationapp/MainActivity.java
+++ app/src/main/java/com/example/applicationapp/MainActivity.java
@@ -22,17 +22,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    ArrayList<News> newsArrayList;
-    MyAdapter myAdapter;
-    NavigationView navigationView;
-    DrawerLayout drawerLayout;
-    FrameLayout frameLayout;
-    MaterialToolbar toolbar;
-
-    String[] newHeading;
-    int[] imageResourceId;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,32 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment() ).commit();
 
-
-        recyclerView = findViewById(R.id.recycleView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-
-        newsArrayList = new ArrayList<News>();
-
-        newHeading = new String[]{
-                "atletism text mainActivity",
-                "basket text mainActivity",
-                "football text mainActivity","innot text mainActivity",
-                "tennis text mainActivity",
-                "volley text mainActivity"
-        };
-        imageResourceId = new int[]{
-                R.drawable.atletism,
-                R.drawable.basket,
-                R.drawable.forbal,
-                R.drawable.inot,
-                R.drawable.tenis,
-                R.drawable.volei,
-
-        };
-
-        getData();
     }
 
 
@@ -93,39 +59,8 @@ public class MainActivity extends AppCompatActivity {
             };
 
 
-    private void getData() {
-        for( int i=0; i<newHeading.length; i++){
-            News news = new News(newHeading[i], imageResourceId[i]);
-            newsArrayList.add(news);
-        }
 
-        myAdapter = new MyAdapter(this, newsArrayList);
-        recyclerView.setAdapter(myAdapter);
 
-        myAdapter.notifyDataSetChanged();
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_item, menu);
-        MenuItem menuItem = menu.findItem(R.id.search_action);
-        SearchView searchView = (SearchView) menuItem.getActionView();
-        searchView.setMaxWidth(Integer.MAX_VALUE);
-        searchView.setQueryHint("Search Here!");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String s) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                myAdapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-        return super.onCreateOptionsMenu(menu);
-    }
 
 
 }
